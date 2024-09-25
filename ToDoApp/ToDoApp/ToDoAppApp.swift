21c4e92db324e3923f500a28941e93da622eb70e
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2024-09-25 23:12:26 +0300

    Add Client iOS side

---
+++ ToDoApp/ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Виктория Серикова on 25.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
